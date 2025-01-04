2357d863282e6c2c934ae09250a2e7a89f60e6b3
huaguihai <huaguihai@example.com>
2025-01-04 12:00:14 +0800

    更新默认推荐码为ol5f44b5b8

--- autoreff.js
+++ autoreff.js
@@ -80,7 +80,7 @@ async function manageMailAndRegister() {
         const ref = await askQuestion('Use my referral code: (y/N): ');
         const referralCode = ref.toLowerCase() === 'n'
             ? await askQuestion('Enter referral code: ')
-            : 'ol2d3a6bea';
+            : 'ol5f44b5b8';
 
         logger(`Register Using Referral code: ${referralCode}`, 'info');
 
